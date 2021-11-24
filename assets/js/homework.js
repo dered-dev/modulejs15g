ea80d45316b876a4fe4451d1f07654030e389acc
dered-dev <jlcc170@gmail.com>
2021-11-23 22:31:23 -0600

    ADD exercise one

---
+++ assets/js/homework.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ * Pedir al usuario por prompt un numero entre 1 y 100
+ * verificar si es un par o impar
+ * imprimir en resultado en consola
+ * Máximo de lineas para este ejercicio, 2 lineas de código
+ * Hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
+ */
+
+/**
+ * 
+ * Pedir al usuario el dia de la semana 
+ * p.ej. lunes, martes, ...
+ * Imprimir en consola el número del dia de la semana equivalente
+ * es decir:
+ * domingo -> 1, lunes -> 2, martes -> 3, etc...
+ */
+
+/**
+ * 
+ * Pedir al usuario 2 numeros 
+ * Imprimir en consola cual es el menor de los 2
+ * Imprimir en consola cual es el mayor de los 2
+ */
