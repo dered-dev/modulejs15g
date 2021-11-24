0a5147abfb411f9af89ca820ae45d961d81009f7
dered-dev <jlcc170@gmail.com>
2021-11-23 20:59:41 -0600

    ADD first exercise

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,2 +1,8 @@
-
-
+/*
+	Ejercicio 1:
+	Pedir 2 variables por prompt
+	Si a > b dividir a entre b
+	Si a < b sumar a más b
+	si a == b multiplicar ambos numeros
+	Imprimir el resultado en consola
+*/
