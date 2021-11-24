046f4fb311f787eb1ef6da996fccfe63044488ef
dered-dev <jlcc170@gmail.com>
2021-11-23 22:28:02 -0600

    ADD exercise one

--- assets/js/base.js
+++ assets/js/base.js
@@ -6,3 +6,42 @@
 	si a == b multiplicar ambos numeros
 	Imprimir el resultado en consola
 */
+
+const a = parseInt(prompt('dame el valor de A'))
+const b = parseInt(prompt('dame el valor de B'))
+
+if(isNaN(a) === false && isNaN(b) === false ){
+    if(a > b ) {
+        console.log(a / b)
+    } else if(a < b){
+        console.log(a + b)
+    } else if(a === b) {
+        console.log(a * b)
+    }
+} else {
+    console.error('Algunos de los numeros no son tipo number')
+}
+
+
+//    console.log('tabla del ' + (i+ 3) + '.') 
+//    console.log(`tabla del ${i+3}`)
+// for(i = 1; i <= 10; i++ ){
+
+//    for(j = 1; j <= 10 ; j++) {
+//     console.log(`${i} x ${j} = ${ i * j }`)
+//    } 
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
