797fe8709c250ed820d7ead4a4531b932237204f
dered-dev <jlcc170@gmail.com>
2021-11-24 20:52:48 -0600

    ADD celsiusGrade solution

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,56 +1,10 @@
-/*
-	Ejercicio 1:
-	Pedir 2 variables por prompt
-	Si a > b dividir a entre b
-	Si a < b sumar a más b
-	si a == b multiplicar ambos numeros
-	Imprimir el resultado en consola
-*/
 
-const a = parseInt(prompt('dame el valor de A'))
-const b = parseInt(prompt('dame el valor de B'))
+// celsius to fahrenheit
+const celsiusGrade = parseInt(prompt('Grados celsius en tu localidad'))
 
-// if(isNaN(a) === false && isNaN(b) === false ){
-//     if(a > b ) {
-//         console.log(a / b)
-//     } else if(a < b){
-//         console.log(a + b)
-//     } else if(a === b) {
-//         console.log(a * b)
-//     }
-// } else {
-//     console.error('Algunos de los numeros no son tipo number')
-// }
-
-!isNaN(a) && !isNaN(b) ? 
-    a > b ? console.log(a / b)
-    : a < b ?  console.log(a + b)
-    : a === b ? console.log(a * b)
-    : ''
-:
-console.error('Algunos de los numeros no son tipo number')
-
-
-
-
-
-
-//    console.log('tabla del ' + (i+ 3) + '.') 
-//    console.log(`tabla del ${i+3}`)
-// for(i = 1; i <= 10; i++ ){
-
-//    for(j = 1; j <= 10 ; j++) {
-//     console.log(`${i} x ${j} = ${ i * j }`)
-//    } 
-
-// }
-
-
-
-
-
-
-
-
-
-
+if(!isNaN(celsiusGrade)){
+	let fahrenheitGrade  = (celsiusGrade * 9/5) + 32
+	console.log(`La temperatura actual es de: ${fahrenheitGrade}ºF`)
+} else {
+	console.log('Es necesario un número')
+}
