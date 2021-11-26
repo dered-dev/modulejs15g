d53064183ffc2664bd5f23a0debf1e80014ef3a4
dered-dev <jlcc170@gmail.com>
2021-11-25 22:47:48 -0600

    ADD homework

---
+++ assets/js/homework.js
@@ -0,0 +1,47 @@
+// pedir al usuario una palabra (permitir espacios, mayusculas)
+// Verificar que sea un palindromo
+// mandar en consola el resultado
+// -> La palabra "anita lava la tina" es un palindromo
+
+
+
+// For solution
+const wordUser = prompt('Dame una palabra ').trim().toLowerCase()
+
+let wordWithoutSpace = ''
+let wordReversed = ''
+
+if(wordUser != ''){
+    // Remove White space
+    for(i = 0; i < wordUser.length; i++){
+        if(wordUser[i] !== ' '){
+            wordWithoutSpace = wordWithoutSpace +  wordUser[i]
+        }
+    }
+    console.log(wordWithoutSpace)
+
+    // word reversed  
+    for(j = wordWithoutSpace.length - 1; j >= 0 ; j--){
+        wordReversed = wordReversed + wordWithoutSpace[j]
+    } 
+    console.log(wordReversed)
+
+    wordWithoutSpace === wordReversed ?
+        console.log(`La palabra "${wordUser}" es un palindromo`)
+    :
+        console.log(`La palabra "${wordUser}" No es un palindromo`)
+
+} else {
+    console.log('Inserta una palabra valida')
+}
+
+
+// Homework
+// De la solucion anterior generar lo siguiente
+// 1. While solution
+// 2. Permitir acentos
+// 
+
+
+// Imprimir en consola la suma de los multiplos de 3 y 5 contenidos entre el 1 y 100
+// -> 233168
