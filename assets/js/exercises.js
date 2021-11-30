91601a9fd372ff6b95d3d00787917b4e4317e9c4
dered-dev <jlcc170@gmail.com>
2021-11-30 13:10:35 -0600

    ADD exercises solution

--- assets/js/exercises.js
+++ assets/js/exercises.js
@@ -2,9 +2,30 @@
 // valor por defecto 10
 // Imprimir en consola las tablas de multiplicar del 1 al 10 
 
+function printMultiTables( tableLimit = 10) {
+    for(i=0; i<= tableLimit; i++){
+        for(j=0; j<= 10; j++){
+            console.log(`${i} x ${j} = ${i * j}`)
+        }
+    }
+}
+
 // Función 2:
 // valor por defecto 10
 // Imprimir en consola la suma de 1 a N, siendo N un numero entre 1 y 100
+
+function addNumbersRange(numberEnd = 10) {
+    let addRange = 0
+    if(!isNaN(parseInt(numberEnd)) || numberEnd >= 1 && numberEnd <= 100){
+        for(i = 1; i <= numberEnd; i++){
+            addRange += i
+        }
+    } else {
+        console.log('No es un número valido')
+    }
+
+    return addRange
+}
 
 // Función 3:
 // valor por defecto "1234"
@@ -12,11 +33,34 @@
 // p.ej. "1234" -> 10
 
 
+function additionNumbers (numberValue = 1234) {
+    let numberToAdd = numberValue.toString()
+    let addNnumber = 0
+    for(i = 0; i < numberToAdd.length; i++){
+        addNnumber += parseInt(numberToAdd[i])
+    }
+
+    return addNnumber
+}
+
 // Función 4:
 // valor por defecto 3 y 5, sino el que ponga el usuario 
 // Imprimir en consola la suma de los múltiplos de 3 y 5 contenidos entre el 1 y 100
 // -> 233168
 
+function getMulti (numberMulti = 'default') {
+    let addMulti = 0
+    if(numberMulti === 'default') {
+        for(i = 0; i <= 100; i++){
+            i % 3 === 0 || i % 5 === 0  ? addMulti += i : ''
+        }
+    } else {
+        for(i = 0; i <= 100; i++){
+            i % numberMulti === 0 ? addMulti += i : ''
+        }
+    }
+    
+    return addMulti
+}
 
-// invertigar Arrow functions
 
