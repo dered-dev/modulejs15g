53a623cd5eacbba3c6a226a23f2161c0902d52dc
dered-dev <jlcc170@gmail.com>
2021-12-01 00:18:30 -0600

    ADD homework solution

--- assets/js/homework.js
+++ assets/js/homework.js
@@ -5,7 +5,19 @@
  * hint: https://www.w3schools.com/jsref/jsref_join.asp
  */
 
+// large solution
+const reverseSentence = (sentence ) => {
+    let newSentence = ''
+    newSentence = sentence.split('').reverse().join('')
+    return newSentence
+}
+// middle solution
+// const reverseSentence = (sentence ) => {
+//     return sentence.split('').reverse().join('')
+// }
 
+// one line solution
+// const reverseSentence = sentence => sentence.split('').reverse().join('')
 
 
 
@@ -15,6 +27,24 @@
  * hint: https://ricardometring.com/javascript-replace-special-characters
  * hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reverse
  */
+
+// large
+const isPalindrome = (sentence) => {
+    let sentenceNormalized = sentence.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+    let sentenceWithoutSpaces = sentenceNormalized.replace(/\s/g,'')
+    let sentenceReversed = sentenceWithoutSpaces.split('').reverse().join('')
+    if(sentenceWithoutSpaces === sentenceReversed){
+        return true
+    } else {
+        return false
+    }
+}
+// short
+// const isPalindrome = sentence => {
+//     let sentenceNormalized = sentence.normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/\s/g,'')
+//     let sentenceReversed = sentenceNormalized.split('').reverse().join('')
+//     sentenceNormalized === sentenceReversed ? true : false
+// }
 
 
 /**
@@ -28,6 +58,32 @@
  * 
 */
 
+const arrayReduce = (arrOne, arrTwo) => {
+    let newArr = arrOne.concat(arrTwo)
+    let arrAdd = 0
+    newArr.forEach( (item) => {
+        arrAdd += item
+    })
+
+    return arrAdd
+}
+
+// middle
+// const arrayReduce = (arrOne, arrTwo) => {
+//     let arrAdd =  0
+//     arrOne.concat(arrTwo).forEach( item => arrAdd += item )
+//     return arrAdd
+// }
+
+// short
+// const arrayReduce = (arrOne, arrTwo) => {
+//     let arrAdd =  0
+//     return arrOne.concat(arrTwo).forEach( item => arrAdd += item )
+// }
+
+// one line solution
+// const arrayReduce = (arrOne, arrTwo) => arrOne.concat(arrTwo).reduce( (acc, cv) => acc += cv )
+
 
 /**
  * Escribir una función que tome como parametro 2 arrays, y devuelva un array 
@@ -35,3 +91,44 @@
  * arrayReduce([1,2,3,4],[1,2,5]). 
  * -> [1,2]
 */
+
+// const getRepeatItems =  (arrOne, arrTwo) => {
+//     let concatArr = arrOne.concat(arrTwo)
+//     let arrRepeated = []
+//     concatArr.forEach((element, index) =>  {
+//         // console.log( concatArr.indexOf(element), index)
+//         if( concatArr.indexOf(element) !== index ) {
+//             arrRepeated.push(element)
+//         }
+//     })
+//     return arrRepeated
+
+// }
+
+// const getRepeatItems =  (arrOne, arrTwo) => {
+//     let arrRepeated = []
+//     arrOne.concat(arrTwo).forEach((element, index) =>  {
+//         concatArr.indexOf(element) !== index ? arrRepeated.push(element) : ''
+//     })
+//     return arrRepeated
+// }
+
+
+// const getRepeatItems =  (arrOne, arrTwo) => {
+//     let arrRepeated = []
+//     arrRepeated = arrOne.concat(arrTwo).filter((element, index, arr) =>  {
+//         if( arr.indexOf(element) !== index ) {
+//             return element
+//         }
+//     })
+
+//     return arrRepeated
+// }
+
+// const getRepeatItems = (arrOne, arrTwo) => {
+//     return arrOne.concat(arrTwo).filter((element, index,arr) =>  {
+//         return arr.indexOf(element) !== index ? element : ''
+//     })
+// }
+
+const getRepeatItems = (arrOne, arrTwo) => arrOne.concat(arrTwo).filter((item, index,arr) => arr.indexOf(item) !== index ? item : '')
