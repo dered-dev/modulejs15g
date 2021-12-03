eda89983ccb7e8721fa7945f7d16604c01a37a5e
dered-dev <jlcc170@gmail.com>
2021-12-02 22:36:56 -0600

    ADD filter and reduce exp

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,103 +1,95 @@
-
-// arr.forEach()
-
-// Ejercicio 1 .forEach()
-// Dado un array con solo números
-// obtener el  valor mas alto
-// input: [1,4,3,2,5]
-// output: -> [5]
-
-const mostBigger = (arr) => {
-	let biggest = arr[0]
-	arr.forEach( (value, index, array) => {
-		if(biggest < value){
-			biggest = value
-		}
-	})
-	return biggest
-}
+/**
+ * arr.filter(function)
+ */
 
 
-const smallerNumber = (arr) => {
-	let smaller = arr[0]
-	
-	arr.forEach( (value) => {
-		if(smaller > value){
-			smaller = value
-		}
-	})
+/**
+ * Dado un array de numeros
+ * Filtrar los que son pares
+ */
 
-	return smaller
-}
-// console.log( smallerNumber( [5,7, 3, 4, 10] ) )
+let arrToFilter = [1,2,3,4,5,6,7]
+// let arrFiltered = arrToFilter.filter(item => item % 2 === 0 )
+let arrFiltered = []
 
+// arrFiltered = arrToFilter.filter( (numero) => {
+// 	if(	numero % 2 === 0 ) {
+// 		console.log(numero)
+// 		return numero
+// 	}
+// })
 
-// arr.map()
-const arrExample = [11,2,3,10,5]
-const resultado = arrExample.map( (val) => {
-	return val * 2
-})
-// console.log(resultado)
-
-// dado un array de numeros,
-// retornar un array con los numeros convertidos a string
-// Entrada : [11,2,3,10,5]
-// Salida : ['11','2','3','10','5']
-
-const numberToString = arr => {
-	arr.map( element => {
-		return element.toString() 
-	})
-}
-
-// const numberToString = arr => arr.map( element => element.toString() )
-
-
-
-
-// .map()
-// función
-// Dado un array como parametro 
-// capitalizar todos los elementos que sean strings
-// typeof variable
-
-// input: ['hOlA', 'mundo', 123]
-// output -> ['Hola','Mundo', 123]
-
-const capitalizeArr = (arr) => {
-
-	let arrCapitalized =  []
-	arrCapitalized = arr.map( (element) => {
-			if(typeof element === 'string') {
-				let stringCap = `${element.slice(0,1).toUpperCase()}${element.slice(1).toLowerCase()}`
-				return stringCap
-			} else {
-				return element
-			}
-    	}
-	)
-	
-	return arrCapitalized
-} 
-
-let arrToCap = ['hOlA', 'mundo', 5, [3,4], 'otro']
-let resultCap = capitalizeArr(arrToCap)
-// console.log(resultCap)
-
-
-
-
-// arr.filter()
-const arrayCitys = ['colima','CDMX','Monterrey', 'Guadalajara']
-
-// del array anterior
-// filtrar solo las ciudades que empiecen con C
-let resultFilter = arrayCitys.filter( (cv,index, arr) => {
-
-	if(cv.toLowerCase().slice(0,1) === 'c'){
-		return cv
+arrFiltered = arrToFilter.filter( (numero, index, arr) => {
+	if(numero % 2 === 0 ) {
+		return numero
 	}
-
 })
 
-console.log(resultFilter)
+// console.log(arrFiltered)
+
+
+/**
+ * 
+ * -> ['hola','mundo', 0, 5]
+ * -> ['hola','mundo']
+ * 1. funcion
+ * 2. filtrar solo los que son de typo string
+ * 
+ */
+
+// const filterStrings = (arr) => {
+// 	let arrStrings = []
+// 	arrStrings = arr.filter((item) => {
+// 		if(typeof item === 'string') {
+// 			return item
+// 		}
+// 	})
+// 	return arrStrings
+// }
+
+// const filterStrings = (arr) => {
+// 	let arrStrings = []
+// 	arrStrings = arr.filter((item) => {
+// 		return typeof item === 'string' ? item : null
+// 	})
+// 	return arrStrings
+// }
+
+// filterStrings(['hola','mundo', 0, 5])
+
+const filterStrings = (arr) => {
+	return arr.filter((item) => typeof item === 'string')
+	.map( (item) => item.toUpperCase())
+	.filter( (item ) => item.slice(0,1) === 'H')
+}
+
+
+
+let arr = [5,2,3]
+let acumulador = 0
+
+// arr.forEach( element => {
+// 	acumulador = acumulador + element
+// })
+
+// console.log(acumulador)
+	
+// arr.reduce()
+let suma = arr.reduce((acc, cv, index, arr)=> {
+	// console.log(acc, cv)
+
+	return acc + cv
+}, 0 )
+
+let arrStr = ['hola', 'mundo']
+let concatStr = arrStr.reduce((acc, cv, index, arr)=> {
+	console.log(acc, cv, index, arr )
+	return acc + cv
+	
+}, 'saludo: ')
+
+console.log(concatStr)
+
+
+
+
