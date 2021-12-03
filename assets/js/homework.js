d74a64479664c12bf4a1fb86e7c3adebc64f5a49
dered-dev <jlcc170@gmail.com>
2021-12-03 02:02:55 -0600

    ADD homework solutions

--- assets/js/homework.js
+++ assets/js/homework.js
@@ -7,10 +7,18 @@
  * 
  */
 
-const mostLarge  = (arr) => {
-    
+ const mostLarge  = (arr) => {
+    let strLarge =  arr[0]
+    arr.forEach( item => {
+        if(strLarge.length <= item.length ){
+            strLarge = item
+        }
+    })
+    return strLarge
 }
 
+let mostLargeStr = mostLarge( ['st','val','min','as'] )
+// console.log(mostLargeStr)
 
 /**
  * arr.map()
@@ -23,8 +31,17 @@ const mostLarge  = (arr) => {
  */
 
 const reverseStringArr  = (arr) => {
-    
+    let reverseString = []
+    reverseString = arr.map( (item) => {
+        return item.split('').reverse().join('')
+    })
+    return reverseString
 }
+// const reverseStringArr = (arr) => {
+//     return arr.map( (item) => item.split('').reverse().join('') )
+// }
+
+// const reverseStringArr = (arr) => arr.map( (item) => item.split('').reverse().join('') )
 
 
 
@@ -36,12 +53,25 @@ const reverseStringArr  = (arr) => {
  * luego, filtre solo los que tengan mas de 5 caracteres
  * y al final, los que tengan al menos 2 letras "a" 
  * 
- * p.ej.  filterStrings( ['guadalajara', 3, 'caracas', 'Oslo', 'brasil', 0] )
+ * p.ej. filterStrings( ['guadalajara', 3, 'caracas', 'Oslo', 'brasil', 0] )
  * Salida -> ['Guadalajara', 'Caracas']
  */
 
 const filterStrings  = (arr) => {
-    
+    return arr.filter(item => typeof item === 'string')
+    .map(item => {
+        return item.slice(0,1).toUpperCase() + item.slice(1).toLowerCase()
+    })
+    .filter( item => {
+        
+        return item.length > 5
+    })
+    .filter( item => {
+        console.log(item)
+        return item.split('a').length > 2
+    })
 }
+
+filterStrings( ['guadalajara', 3, 'caracas', 'Oslo', 'brasil', 0] ) 
 
 
