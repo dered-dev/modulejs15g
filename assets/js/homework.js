39cbda1eb5dd8a0a8480b8b265777350f0a53e97
dered-dev <jlcc170@gmail.com>
2021-12-04 01:56:52 -0600

    ADD homework

--- assets/js/homework.js
+++ assets/js/homework.js
@@ -1,33 +1,3 @@
-
-
-// jorgec@kodemia.mx
-// 1. saber hasta donde terminar de reemplazar 6
-// --- .indexOf(searchElement)
-// 2. por que se va a reemplazar? *
-// 3. Reemplazar 
-
-let sep = 'jorgec@kodemia.mx'.split('@')
-let maskStr = ''
-
-// sep[0].split('').forEach(() => {
-//     maskStr += '*'
-// })
-
-for(i = 0; i < sep[0].length ; i++){
-    maskStr += '*'
-}
-// console.log(`${maskStr}@${sep[1]}`)
-
-
-
-
-
-
-
-
-
-
-
 /** 
  * Dado un arreglo con nombres de personas, 
  * devuele un arreglo con los nombres de las personas que empiezan con vocales (capitalizados)
