c5b87175ee423284f16d58dcf94b73b138aa63b7
dered-dev <jlcc170@gmail.com>
2021-12-08 21:00:12 -0600

    ADD solutions methods

--- assets/js/objects.js
+++ assets/js/objects.js
@@ -1,241 +1,77 @@
 
-// elemento.metodo(parametros)
-// Object.keys(obj)
-let salarios  = {
-    'jorge':  3000,
-    'juan':  7000,
-    'Pedro':  8000
-}
+// Metodos de objetos
 
+// console.log(this)
+// console.log(this.alert)
 
-
-
-
-let empleados = []
-// for( item in salarios){
-//     empleados.push(item)
-// }
-
-// console.log( Object.keys(salarios) )
-
-// funcion
-// recibe un objeto
-// retornar el total de los salarios
-
-// hint : reduce
-// hint : forEach
-
-const sumarSalarios = (obj) => {
-    let suma = 0
-    Object.keys(obj).forEach( (empleado, index, arr) => {
-
-        let salario = obj[empleado]
-        suma = suma + salario
-        // suma += salario
-    })
-    return suma
-}
-
-
-// console.log(sumarSalarios(salarios))
-
-
-const sumarSalariosRed = (obj) => {
-    let suma = 0
-    suma = Object.keys(obj).reduce( (acc, key) => {
-        return acc + obj[key]
-    }, 0)
-    return suma
-}
-
-// console.log(sumarSalariosRed(salarios))
-// let salarios  = {
-//     'jorge':  3000,
-//     'juan':  7000,
-//     'Pedro':  3000
-// }
-
-/*
-{
-    llave: valor,
-    llave: valor,
-    llave: valor,
-}
-*/
-// .reduce()
-
-const addArr = (obj) => {
-
-    let suma = 0
-
-    suma = Object.values(obj).reduce((previousValue, currentValue) => {
-        return previousValue += currentValue
-    }, 0)
-
-    return suma
-}
-
-// console.log(addArr(salarios))
-
-const addArrOneLine = (obj) => Object.values(obj).reduce((acc, cv) => acc += cv, 0)
-// console.log(addArrOneLine(salarios))
-
-// Object.assign() -> concat
-
-let obj1 = {
-    'nombre': 'jorge luis',
-    'isMentor': true,
-    'age': 19
-}
-// console.log(obj1)
-let obj2 = {
-    apellido: 'Camarillo'
-}
-
-// Object.assign(target, ...sources)
-Object.assign(obj1,obj2)
-// console.log(obj1)
-
-
-// let obj1 = {
-//     nombre: 'jorge luis',
-//     'isMentor': true,
-//     apellido: 'Camarillo'
-// }
-
-
-console.log( Object.entries(obj1) )
-// console.log(obj1)
-// [
-//     ['key', 'value'],
-//     ['key','value' ]
-// ]
-
-// [
-//     ['nombre', 'jorge luis'],
-//     ['apellido','Camarillo']
-// ]
-
-obj1.generation = 6
-// Object.freeze(obj1)
-obj1.generation = 7
-// console.log(obj1)
-delete obj1.generation
-// console.log(obj1)
-
-
-
-
-// destructuracion de arrays
-let newArr = [10, 20, 30, 40, 50]
-// let a = newArr[0]
-// let b = newArr[1]
-// let c = newArr[2]
-// let d = newArr[3]
-// let [a, b, c, d, e] = newArr
-// console.log(a, b, c, d, e)
-// console.log(newArr)
-
-
-// let [a, b] = [10, 20, 30, 40, 50]
-// console.log(a)
-// console.log(b)
-
-let [a, b, ...rest] = [10, 20, 30, 40, 50]
-// console.log(a)
-// console.log(b)
-// console.log(...rest)
-
-let [firstName, lastName, age] = ['Jorge', 'camarillo', 30]
-// console.log(firstName)
-// console.log(lastName)
-// console.log(age)
-
-let destrucArr = [
-    ['Bradley', 'Bouley'],
-    ['Chloe', 'Alnaji'],
-    ['Jonathan', 'Baughn'],
-    ['Michael', 'Herman'],
-    ['Robert', 'Hajek'],
-    ['Wes', 'Reid'],
-    ['Zach', 'Klabunde']
-]
-
-destrucArr.forEach((value, index, array) => {
-    let [ nombre, apellido ] = value
-    console.log(`${nombre} ${apellido}`)
-})
-
-
-// destructuracion de objetos
-const hero = {
-    name: 'Batman',
-    realName: 'Bruce Wayne',
-    otherProp: {
-        prop: 'value',
+let koder = {
+    name:"jorge",
+    lastName:"Camarillo",
+    average: [10, 10, 10, 8, 9],
+    // totalAverage: 10,
+    // totalAverage: 9.4, 
+    // getFullName: function () {
+    //     console.log(this)
+    //     return `${ this.name } ${ this.lastName } ${this.age}`
+    // }
+    setAge: function (age = 10) {
+        this.age = age
     }
 }
-const { name, realName } = hero
-const { name: firstNameObj, realName: legalName, otherProp } = hero
+// console.log(koder)
 
-console.log(name, realName)
-console.log(firstNameObj, legalName, otherProp)
+// koder.setAge()
 
-
-let users = [
-    { firstName: 'Bradley', lastName: 'Bouley', role: 'Full Stack Resident' },
-    { firstName: 'Chloe', lastName: 'Alnaji', role: 'Full Stack Resident' },
-    { firstName: 'Jonathan', lastName: 'Baughn', role: 'Enterprise Instructor' },
-    { firstName: 'Michael', lastName: 'Herman', role: 'Lead Instructor' },
-    { firstName: 'Robert', lastName: 'Hajek', role: 'Full Stack Resident' },
-    { firstName: 'Wes', lastName: 'Reid', role: 'Instructor'},
-    { firstName: 'Zach', lastName: 'Klabunde', role: 'Instructor'}
-]
-
-users.forEach((value, index, array) => {
-
-    let {
-        firstName, 
-        lastName, 
-        role
-    } = value
-
-    console.log(firstName, lastName, role)
-    // console.log(value.firstName, value.lastName, value.role)
-})
+// console.log(koder)
 
 
 
-
-
-// const { nameHero,  realName: secretName } = hero
-// console.log(secretName)
-
-// spread operator
-
-const arr1 = ['one', 'two']
-const arr2 = [...arr1, 'three','four', 'five']
-console.log(arr2)
-
-
-let objspread1 = {
-    propiedad: 'valor',
-    age: 23
-}
-let objspread2 = {
-    propiedad2: 'valor2',
-    ...objspread1
+koder.getFullName = function () {
+    console.log(this)
+    return `${ this.name } ${ this.lastName }`
 }
 
-console.log(objspread2)
+
+/**
+ * Ejercicio
+ * Asignar un nuevo metodo al objeto koder
+ * que devuelva el promedio general
+ * @returns Number
+ * 
+ * p.ej. console.log(koder.getAverage())
+ * -> 9.4
+ * 
+ */
+
+koder.getAverage = function () {
+    let sum =  this.average.reduce((acc, cv) => acc += cv, 0)
+    let total =  this.average.length
+    let av = sum / total
+    return av
+}
+
+koder.setAverage = function () {
+    this.totalAverage = this.getAverage()
+}
+
+// Agregar un metodo para calcular el IMC
+koder.setGetIMC = function () {
+    
+}
+
+// Agregar un metodo para Agregar un nuevo promedio
+koder.setNewAve = function () {
+    
+}
 
 
+// Agregar un metodo para agregar una nueva propiedad 
+koder.addNewProperty = function () {
+    
+}
 
-
-// 2 ejercicios
-// rest operator
-
-
+koder.newProp = ''
+koder['newProp'] = ''
 
 
 
