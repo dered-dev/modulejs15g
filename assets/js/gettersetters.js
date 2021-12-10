6b93cdae38058311e023b03da086234217ed8246
dered-dev <jlcc170@gmail.com>
2021-12-09 19:44:45 -0600

    ADD solutions homework

--- assets/js/gettersetters.js
+++ assets/js/gettersetters.js
@@ -23,7 +23,7 @@ let koder = {
     set fullName(objFullName) {
         this.name = objFullName[0]
         this.lastName = objFullName[1]
-    },
+    }
 }
 
 // Metodos
@@ -48,28 +48,43 @@ koder.fullName = ['jorge luis', 'camarillo']
  * 
  */
 
+
+/*
+ Object.defineProperty( objeto , 'nuevapropiedad', {
+    get() {
+        return this.average.reduce((acc, cv) => acc += cv, 0) / this.average.length
+    }
+})
+*/
+
 // 1. getter average
 Object.defineProperty( koder , 'averagekoder', {
     get() {
         return this.average.reduce((acc, cv) => acc += cv, 0) / this.average.length
     }
 })
-console.log(koder.averagekoder)
+// console.log(koder.averagekoder)
 
 
 // 2. getter, setter IMC
+
 koder.weight = 90
-koder.height = 1.73 
+koder.height = 1.73
+// console.log(koder)
+
 Object.defineProperty( koder , 'IMCKoder', {
     get() {
         return this.weight / (this.height * this.height)
     },
     set(value) {
+        console.log(value)
       this.IMC = value
     }
 })
-console.log(koder.IMCKoder)
-koder.IMCKoder = koder.IMCKoder
+// console.log(koder.IMCKoder)
+
+let imcKoder = koder.IMCKoder
+koder.IMCKoder = imcKoder
 
 
 // 3. setter Score
@@ -79,18 +94,37 @@ Object.defineProperty( koder , 'score', {
     }
 })
 
-koder.score = 6
+koder.score = 8
 
 // 4. setter newProperty
 Object.defineProperty( koder , 'newProperty', {
     set(objProp) {
+
+        console.log(objProp)
+        // koder['nueva'] = 'valor'
         this[objProp.nameProperty] = objProp.valueProperty
     }
 })
 
+
 koder.newProperty = {
-    nameProperty: 'Nueva propiedad',
-    valueProperty: 'nuevo valor'
+    nameProperty: 'gender',
+    valueProperty: 'M'
+}
+
+koder.newProperty = {
+    nameProperty: 'generation',
+    valueProperty: 6
+}
+
+koder.newProperty = {
+    nameProperty: 'random',
+    valueProperty: true
+}
+
+koder.newProperty = {
+    nameProperty: 'random2',
+    valueProperty: [1,9,7]
 }
 
 /**
