33db28b8cf201e2413bec285841f4ea24b84fc8f
dered-dev <jlcc170@gmail.com>
2021-12-10 00:16:19 -0600

    ADD homework

--- assets/js/base.js
+++ assets/js/base.js
@@ -81,27 +81,40 @@ document.getElementsByTagName('body')[0].appendChild(ulElement)
 // body -> <ul> <li>item 1</li> <li>item 2</li> </ul>
 
 
-
-
-
-
-
-
-
-
-
-
-
-// data attributes
-// propiedades personalizadas
-
-
-
 /**
- * Seleccionar padres e hijos (traversing)
+ * Ejercicio 1
+ * <ul id="menu" class="menu">
+        <li class="item__menu">Home</li>
+        <li class="item__menu">Products</li>
+        <li class="item__menu">About Us</li>
+    </ul>
+
+    Replicar con JS este markup
  */
 
 
-/**
- * Manipular elementos del DOM
- */
+let ulMenu = document.createElement('ul')
+ulMenu.classList.add('menu')
+ulMenu.setAttribute('id','menu')
+
+let liMenuHome = document.createElement('li')
+liMenuHome.classList.add('item__menu')
+liMenuHome.textContent = 'Home'
+ulMenu.appendChild(liMenuHome)
+
+let liMenuProducts = document.createElement('li')
+liMenuProducts.classList.add('item__menu')
+liMenuProducts.textContent = 'Products'
+ulMenu.appendChild(liMenuProducts)
+
+let liMenuAboutUs = document.createElement('li')
+liMenuAboutUs.classList.add('item__menu')
+liMenuAboutUs.textContent = 'About Us'
+ulMenu.appendChild(liMenuAboutUs)
+
+document.getElementById('body').appendChild(ulMenu)
+
+
+
+
+
---
+++ assets/js/homework.js
@@ -0,0 +1,60 @@
+// Tarea 1
+
+/**
+ * Ejercicio 1.
+ * Dado el siguiente Array [Hoteles, Ofertas, Viajes, Ayuda, Cancelaciones]
+ * Formar el siguiente markup en el DOM
+ * <ul class="menu__booking" data-menu="booking">
+        <li class="menu__booking">Hoteles</li>
+        <li class="menu__booking">Ofertas</li>
+        <li class="menu__booking">Viajes</li>
+        <li class="menu__booking">Ayuda</li>
+        <li class="menu__booking">Cancelaciones</li>
+    </ul>
+ * 
+ */
+
+
+
+
+/**
+ * Ejercicio 2.
+ * Dado el siguiente Array de objetos "objPromesas"
+ * Crear una funcion para Formar el markup necesario para que se vea asi
+ * https://ibb.co/hWSvpx5
+ * 
+ * NOTA: recuerda agregas el CSS necesario para lograr este objetivo
+ * 
+ */
+
+ let objPromesas = [
+    {
+        imagen: 'https://imagenescityexpress.scdn6.secure.raxcdn.com/sites/default/files/2021-07/hotel1.png',
+        titulo: 'LA MAYOR COBERTURA',
+    },
+    {
+        imagen: 'https://imagenescityexpress.scdn6.secure.raxcdn.com/sites/default/files/2021-07/icono_desayuno_cortesia_2_0.png',
+        titulo: 'DESAYUNO EN CORTESÍA*',
+    },
+    {
+        imagen: 'https://imagenescityexpress.scdn6.secure.raxcdn.com/sites/default/files/2021-07/icono_cancelacion_flexible_0.png',
+        titulo: 'CANCELACIÓN SIN COSTO**',
+    },
+    {
+        imagen: 'https://imagenescityexpress.scdn6.secure.raxcdn.com/sites/default/files/2021-07/icono_wifi_cortesia_2_0.png',
+        titulo: 'WIFI EN CORTESÍA',
+    }
+]
+
+/**
+ *  Tarea 2
+ * Estudiar lo siguiente:
+ * 1. innerHTML
+ * 2. insertBefore()
+ * 3. insertAfter()
+ * 4. append()
+ * 5. prepend()
+ * 6. removeChild()
+ * 7. DOM events
+ * 
+ */
