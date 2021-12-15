14a3aeb6257d7199a3886e2199c8a10975b03e5d
dered-dev <jlcc170@gmail.com>
2021-12-14 19:39:26 -0600

    ADD exercise

--- assets/js/eventlistener.js
+++ assets/js/eventlistener.js
@@ -1,18 +1,30 @@
 
-// .addEventListener('type', listener, options)
+// .addEventListener('type', callback)
 // callback JS
-
-const saludo = () => {
-    console.log('saludo event listener')
-} 
+// type: click, change, focus, mouseleave, mouseenter
+/**
+ * focus, focusin, focusout
+ * mouseenter, mouseleave, mousemove
+ * click, rightclick
+ * 
+ * teclado
+ * keydown, keypress, keyup
+ * focus
+ * 
+ * load, beforeload
+ * 
+ */
 
 let buttonEvent = document.getElementById('button__eventListener')
 
+const saludo = () => {
+    console.log('saludo event listener ok')
+} 
+
 // add event
-buttonEvent.addEventListener('click', saludo)
-// buttonEvent.addEventListener('click', () => {
-//     console.log('saludo event listener')
-// })
+// buttonEvent.addEventListener('click', saludo )
+
+buttonEvent.addEventListener('click', () => { console.log('saludo event listener') })
 
 // other event
 document.getElementById('button__eventListener').addEventListener('focus', () => {
@@ -21,5 +33,36 @@ document.getElementById('button__eventListener').addEventListener('focus', () =>
 
 // remove event
 buttonEvent.removeEventListener('click',saludo)
+
+
+// ejercicio
+// Agregar a 5 koders mas
+// pintarlos en el DOM
+// 1. utilizar bootstrap
+// 2. Que sea responsive
+// 3. minimo 3 cards por row
+
+let koders = [
+    {
+        name: 'Emilio',
+        age: 30,
+        city: 'Guadalajara',
+        generacion: 6,
+        typeKoder: 'js',
+        gender: 'M',
+        avatar: 'https://picsum.photos/200/200'
+    },
+    {
+        name: 'Juan',
+        age: 30,
+        city: 'Ciudad de méxico',
+        generacion: 6,
+        typeKoder: 'iOS',
+        gender: 'M',
+        avatar: 'https://picsum.photos/200/200'
+    }
+]
+
+
 
 
