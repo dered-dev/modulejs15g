195341ed5331a61c87ab9355e6b766e6fe954eb0
dered-dev <jlcc170@gmail.com>
2021-12-15 21:09:50 -0600

    ADD ajax XHR

--- assets/js/Ajax.js
+++ assets/js/Ajax.js
@@ -36,29 +36,39 @@
 const xhttp = new XMLHttpRequest()
 // Abrir la conexion (empezar a creat el request)
 // xhttp.open(metodo, url)
-xhttp.open( "GET" , "https://jsonplaceholder.typicode.com/posts/1", true)
+xhttp.open( "GET" , "https://jsonplaceholder.typicode.com/posts", true)
 
 // Escuchar cuando el response este listo
 xhttp.onload = function(data) {
 
     if(data.target.status === 200){
-        let res = data.target.response
-        let objResp = JSON.parse(res)
+        let objResp = JSON.parse(data.target.response)
+        console.log(objResp)
         // imprimir en DOM
         let templateUser = ''
-        templateUser = `
-        <div>
-            <h2>${objResp.title}</h2>
-            <p>${objResp.body}</p>
-        </div>
-        `
-        document.querySelector('.container').innerHTML = templateUser
+        objResp.forEach( (post) => {
+
+            templateUser += `
+            <div class="col-12 col-md-4 mb-4">
+                <div class="card bg-light" >
+                    <div class="card-body">
+                        <h5 class="card-title">${post.title}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${post.id}</h6>
+                        <p class="card-text">${post.body}</p>
+                    </div>
+                </div>
+            </div>
+            `
+        })
+        document.querySelector('.grid__posts').innerHTML = templateUser
     }
     
 }
 
 // Enviar la peticion
 xhttp.send()
+
+console.log('espera terminada')
 
 /**
  * 
