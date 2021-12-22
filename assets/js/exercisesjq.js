1dbc70a1f68f82c7ba012d0e1fcb684cdc70f718
dered-dev <jlcc170@gmail.com>
2021-12-21 20:47:25 -0600

    ADD exercise solution

--- assets/js/exercisesjq.js
+++ assets/js/exercisesjq.js
@@ -1,4 +1,24 @@
 
 $(document).ready(function () {
-    console.log('ready')
+    let temp = ''
+    for(i = 5; i <= 14; i++) {
+        temp += `<li>item ${i}</li>`
+    }
+    $('ul').append(temp)
+    $('ol').prepend(temp)
+
+    $('.content').html('<p>Hola koder</p>')
+
+    $('.content').css({
+        'background-color': '#333',
+        'color': '#fff'
+    })
+
+    $('body').prepend('<h1>Empty</h1>')
+    $('body h1').text('Aprendiendo jQuery')
+
+
+
+    
+
 })
