587cea84d2ee1def9cd542366de7bbaeed079665
dered-dev <jlcc170@gmail.com>
2021-12-21 20:23:00 -0600

    ADD jquery exercises

--- assets/js/homework.js
+++ assets/js/homework.js
@@ -9,7 +9,8 @@
 
 
 /**
- * 1. Comprobar que jQuery esta cargado
- * 2. Crear un elemento “div” en el html y con jQuery agregarle la clase ‘’list”
- * 3. Crear un elemento “ol” con 5 items “li” dentro, y con jQuery agregarles la clase ‘’list__item”
+ * 1. ok: Comprobar que jQuery esta cargado
+ * 2. ok: Crear un elemento “div” en el html y con jQuery agregarle la clase ‘’list”
+ * 3. Crear un elemento “ol” con 5 items “li” dentro, 
+ * y con jQuery agregarles la clase ‘’list__item”
  */
--- assets/js/jqueryclass.js
+++ assets/js/jqueryclass.js
@@ -1,6 +1,6 @@
 
 // document.getElementsByTagName('h1')[0].style.display = 'none'
-$('h1').hide()
+// $('h1').hide()
 // jQuery('element').addClass('hola')
 
 // window.addEventListener('load', () => {})
@@ -34,11 +34,53 @@ $('h1').hide()
  * .keydown()
  * 
  */
-$('p').addClass('parrafo')
-$('p').removeClass('texto')
-$('p').toggleClass('texto')
-$('p').hide()
-$('p').show()
-$('p').remove()
+// $('p').addClass('parrafo')
+// $('p').removeClass('texto')
+// $('p').toggleClass('texto')
+// $('p').hide()
+// $('p').show()
+// $('p').remove()
+
+
+
+
+
+$(document).ready( () => {
+    console.log('document ready')
+    $('div').addClass('list')
+
+    $('ol li').addClass('list_item')
+
+    // getters
+    $('h1').attr('style')
+
+    // setter 
+    $('h1').attr('data-id','5')
+    $('h1').attr('id','heading')
+
+    $('h2:first').attr('class','heading')
+
+    let ultimoHIjo = '<li class="item_final">item list</li>'
+    let primerHIjo = '<li class="item_inicial" >item list</li>'
+    $('ol').append(ultimoHIjo)
+    $('ol').prepend(primerHIjo)
+
+
+    // innerText
+    $('.item_final').text('Item final')
+    // innerHTML
+    $('.inner__html').html(' <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus non corporis deleniti modi temporibus eum aperiam vitae laudantium aut ex voluptate ab, sapiente totam ipsa similique! Dolorem dicta eaque eos.</p>')
+
+    // Atributo Style
+    // 1 propiedad css
+    $('.item_final').css('color','red')
+    // multiples propiedad css
+    $('.item_final').css({
+        'font-weight':'600',
+        'font-size':'32px',
+        'font-style':'italic',
+    })
+
+})
 
 
